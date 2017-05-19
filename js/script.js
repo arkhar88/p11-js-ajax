ac8c718e218be64a57347af972cb8b167e3ae789
arkhar88 <arkhar88@gmail.com>
2017-05-19 13:30:11 +0200

    Tweer qoute

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
-var url = 'http://api.icndb.com/jokes/random';
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
-var btn = document.getElementById('get-joke');
-btn.addEventListener('click', function() {
-    getJoke();
-});
-
-var par = document.getElementById('joke');
-
-function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function() {
-        var response = JSON.parse(xhr.response);
-        par.innerHTML = response.value.joke;
+function getQuote() {
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({
+        cache: false
     });
-    xhr.send();
 }
 
-$(function() {
-    getJoke();
+function createTweet(input) {
+    var data = input[0];
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+        console.log(quoteText);
+    }
+}
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    });
 });
